6be7b1a825930738093c18cce3d3c1fff501db44
Kashyap Vinodbhai Nasit <kashyap.nasit@myntra.com>
2023-06-20 00:39:40 +0530

    Test to github action

--- src/main/java/com/nasit/Main.java
+++ src/main/java/com/nasit/Main.java
@@ -2,6 +2,6 @@ package com.nasit;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World to Github Action");
     }
 }
